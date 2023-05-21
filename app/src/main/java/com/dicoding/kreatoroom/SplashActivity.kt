f75a0173998965c4591f0f54a86b3c4c3ac7488a
unknown <ryu.faryan@binus.ac.id>
2023-05-21 21:35:26 +0700

    Beginning of Works

---
+++ app/src/main/java/com/dicoding/kreatoroom/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.dicoding.kreatoroom
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 4000)
+    }
+}
