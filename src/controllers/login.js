57fec9539514fec91d05575460fb48d7f6e1f8da
ssrakuen <akmalnafis999@gmail.com>
2023-05-22 22:48:27 +0700

    repaired the login endpoint

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -7,41 +7,42 @@ pool.on('error',(err)=> {
     console.error(err);
 });
 
-module.exports ={
+module.exports = {
     //this is a login page routing example, needs further research
     getLoginPage(req, res){
         const loginPagePath = path.join(__dirname, '../views/login-page.html');
         res.sendFile(loginPagePath);
     },
-    postLoginData(req, res){
+    postLoginData(req, res) {
         const data = {
-            email : req.body.email,
-            password : req.body.password
+          email: req.body.email,
+          password: req.body.password
         };
         if (!data.email || !data.password) {
-            return res.send({
-                status: 400,
-                success: false,
-                message: 'Username and password must be provided!'
-            });
+          return res.send({
+            status: 400,
+            success: false,
+            message: 'Email dan password harus diisi!'
+          });
         }
-        pool.getConnection(function(err, connection) {
-            if (err) throw err;
-            connection.query(
-                'SELECT * FROM users WHERE email = ?',
-                [data.email],
-                function (error, results) {
-                    if (error) throw error;
-                    if (results.length === 0 || results[0].password !== data.password) {
-                        return res.status(401).json({ message: 'Invalid username or password' });
-                    }
-                    res.send({
-                        success: true,
-                        message: 'Berhasil tambah data!',
-                    });
-                }
-            );
-            connection.release();
+        pool.getConnection(function (err, connection) {
+          if (err) throw err;
+          connection.query(
+            'SELECT * FROM user WHERE email = ?',
+            [data.email],
+            function (error, results) {
+              if (error) throw error;
+              if (results.length === 0 || results[0].password !== data.password) {
+                return res.status(401).json({ message: 'Invalid username or password' });
+              }
+              res.send({
+                status: 200,
+                success: true,
+                message: 'Berhasil login!'
+              });
+            }
+          );
+          connection.release();
         });
     },
 }
