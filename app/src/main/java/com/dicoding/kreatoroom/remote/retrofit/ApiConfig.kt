3969a9a93a52e60a709527f4f7416ee301b639e9
rwznt <faryanryu@gmail.com>
2023-05-26 00:19:37 +0700

    XML Register Renewal + Added ApiConfiguration Temporarily

--- app/src/main/java/com/dicoding/kreatoroom/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/dicoding/kreatoroom/remote/retrofit/ApiConfig.kt
@@ -1,4 +1,41 @@
 package com.dicoding.kreatoroom.remote.retrofit
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiConfig {
+    private var token: String? = null
+
+    fun getApiService(): ApiService {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val interceptor = Interceptor { chain ->
+            val req = chain.request()
+            if (token != null) {
+                val requestHeader = req.newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(requestHeader)
+            } else {
+                chain.proceed(req)
+            }
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(interceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("URL_BASE")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+
 }
