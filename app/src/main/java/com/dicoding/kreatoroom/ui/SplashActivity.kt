80c533aee62528f982df0d8c5a1e8e1800686f37
rwznt <faryanryu@gmail.com>
2023-05-26 00:22:17 +0700

    XML Register Renewal + Added ApiConfiguration Temporarily

--- app/src/main/java/com/dicoding/kreatoroom/ui/SplashActivity.kt
+++ app/src/main/java/com/dicoding/kreatoroom/ui/SplashActivity.kt
@@ -4,22 +4,32 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.kreatoroom.R
+import com.dicoding.kreatoroom.databinding.ActivitySplashBinding
 import com.dicoding.kreatoroom.ui.home.MainActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
 
-        setContentView(R.layout.activity_splash)
+        Thread{
+            var counting = 0
+            while(counting <= 100) {
+                counting += 10
+                Thread.sleep(300L)
+            }
+        }.start()
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 4000)
+        }, 4000L)
     }
 }
