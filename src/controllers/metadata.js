154a0825ca69788fe14bbeca3d4d01ad9e8e1ad3
ssrakuen <akmalnafis999@gmail.com>
2023-05-31 00:28:30 +0700

    metadata API initiation

---
+++ src/controllers/metadata.js
@@ -0,0 +1,28 @@
+const config = require('../configs/database');
+const mysql = require('mysql');
+const pool = mysql.createPool(config);
+
+pool.on('error', (err) => {
+  console.error(err);
+});
+
+module.exports = {
+  getBarangData(req, res) {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(
+        'SELECT * FROM barang;',
+        function (error, results) {
+          if (error) throw error;
+          res.send({
+            status: 200,
+            success: true,
+            message: 'Barang data retrieved successfully',
+            data: results,
+          });
+        }
+      );
+      connection.release();
+    });
+  },
+};
