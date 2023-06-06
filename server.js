1b07f71d103ab98217c12c24348a90edb16d0573
KhoirurRo <113230989+KhoirurRo@users.noreply.github.com>
2023-06-07 01:21:18 +0700

    Auth again

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 require('dotenv').config();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -15,34 +15,58 @@ module.exports = {
   postLoginData(req, res) {
     const data = {
       email: req.body.email,
-      password: req.body.password,
+      userid: req.body.userid,
+      name: req.body.name,
     };
-    if (!data.email || !data.password) {
-      return res.send({
-        status: 400,
-        success: false,
-        message: 'Email dan password harus diisi!',
-      });
-    }
+    // if (!data.email || !data.password) {
+    //   return res.send({
+    //     status: 400,
+    //     success: false,
+    //     message: 'Email dan password harus diisi!',
+    //   });
+    // }
     pool.getConnection(function (err, connection) {
-    if (err) throw err;
+      if (err) throw err;
       connection.query(
         'SELECT * FROM user WHERE email = ?',
         [data.email],
-        function (error, results) {
-          if (error) throw error;
-          if (results.length === 0 || results[0].password !== data.password) {
-            return res
-              .status(401)
-              .json({ message: 'Invalid username or password' });
+        (err, rows) => {
+          if (err) {
+            console.log(err);
+          } else if (rows.length > 0) {
+            res.send({
+              message: 'User alredy exists',
+            });
+          } else {
+            connection.query(
+              'INSERT INTO user (id_pengguna, email, nama_lengkap) VALUES (?,?,?)',
+              [data.userid, data.email, data.name]
+            );
+            res.send({
+              message: 'User berhasil ditambahkan',
+              data: data,
+            });
           }
-          res.send({
-            status: 200,
-            success: true,
-            message: 'Berhasil login!',
-          });
         }
       );
+      // connection.query(
+      //   'SELECT * FROM user WHERE email = ?',
+      //   [data.email],
+      //   function (error, results) {
+      //     if (error) throw error;
+      //     // if (results.length === 0 || results[0].password !== data.password) {
+      //     if (results.length === 0) {
+      //       return res
+      //         .status(401)
+      //         .json({ message: 'Invalid username or password', results });
+      //     }
+      //     res.send({
+      //       status: 200,
+      //       success: true,
+      //       message: 'Berhasil login!',
+      //     });
+      //   }
+      // );
       connection.release();
     });
   },
