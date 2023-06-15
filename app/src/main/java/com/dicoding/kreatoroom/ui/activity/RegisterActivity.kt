102fef6abb53bd93df50aba6d1996b4aa190afc7
ardiwinataa <ardi.winata@binus.ac.id>
2023-06-15 16:11:51 +0700

    register

--- app/src/main/java/com/dicoding/kreatoroom/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/kreatoroom/ui/activity/RegisterActivity.kt
@@ -1,18 +1,56 @@
 package com.dicoding.kreatoroom.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.kreatoroom.databinding.ActivityRegisterBinding
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.storyappapi2.databinding.ActivityRegisterBinding
+import com.example.storyappapi2.utils.Resource
+import com.example.storyappapi2.viewmodel.RegisterViewModel
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private val registerViewModel by viewModels<RegisterViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
+        binding.arahKeLogin.setOnClickListener{
+            Intent(this@RegisterActivity, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        binding.buttonRegister.setOnClickListener {
+            val name = binding.edRegisterName.text.toString().trim()
+            val email = binding.edRegisterEmail.text.toString().trim()
+            val password = binding.edRegisterPassword.text.toString().trim()
+
+            if(name.isEmpty() && email.isEmpty() && password.isEmpty()){
+                binding.edRegisterName.error = "Nama kosong"
+                binding.edRegisterEmail.error = "Email kosong"
+                binding.edRegisterPassword.error = "Password kosong"
+            } else{
+                registerViewModel.userRegister(name, email, password)
+                registerViewModel.register.observe(this){
+                    when(it){
+                        is Resource.Success -> {
+                            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+
+                        }
+                        is Resource.Error -> {
+                            Toast.makeText(
+                                this, it.message.toString(), Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/dicoding/kreatoroom/ui/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/dicoding/kreatoroom/ui/viewmodel/RegisterViewModel.kt
@@ -1,4 +1,46 @@
 package com.dicoding.kreatoroom.ui.viewmodel
 
-class RegisterViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dicoding.kreatoroom.ui.data.RegisterResponse
+import com.example.storyappapi2.api.ApiClient
+import com.example.storyappapi2.utils.Resource
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RegisterViewModel: ViewModel() {
+    private val _register = MutableLiveData<Resource<RegisterResponse>>()
+    val register: LiveData<Resource<RegisterResponse>> = _register
+
+    fun userRegister(name: String, email: String, password: String){
+        val client = ApiClient.apiInstance.userRegister(name,email,password)
+        Log.d("TAG", "userLogin: $client")
+        client.enqueue(object : Callback<RegisterResponse>{
+            override fun onResponse(
+                call: Call<RegisterResponse>,
+                response: Response<RegisterResponse>
+            ) {
+                if(response.isSuccessful){
+                    _register.value = response.body()?.let{ Resource.Success(it) }
+                    Log.d("RegisterViewModel", "onResponse: ${_register.value}")
+                }
+                else {
+                    val errorBody = response.errorBody()?.string()
+                    val errorMessage = errorBody?.let{
+                        JSONObject(it).getString("message")
+                    }
+                    _register.value = Resource.Error(errorMessage)
+                    Log.e("RegisterViewModel", "onResponse: $errorMessage")
+                }
+            }
+            override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+                _register.value = Resource.Error("${t.message}")
+                Log.e("RegisterViewModel", "onFailure: ${t.message}")
+            }
+        })
+    }
 }
